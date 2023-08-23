0bea02f07068abbc72471a91174c0d421ad6dfe2
Nahuelcapo2001 <nahuel.sandovalcapo@gmail.com>
2023-08-23 15:35:17 -0300

    Actualizacion

---
+++ src/Ejercicio2/Producto.java
@@ -0,0 +1,21 @@
+package Ejercicio2;
+
+import java.util.Date;
+
+public class Producto {
+	private Date fechaCaducidad;
+	private int numeroLote;
+	
+	public Date getFechaCaducidad() {
+		return fechaCaducidad;
+	}
+	public void setFechaCaducidad(Date fechaCaducidad) {
+		this.fechaCaducidad = fechaCaducidad;
+	}
+	public int getNumeroLote() {
+		return numeroLote;
+	}
+	public void setNumeroLote(int numeroLote) {
+		this.numeroLote = numeroLote;
+	}
+}
---
+++ src/Ejercicio3/IInstalacionDeportiva.java
@@ -0,0 +1,6 @@
+package Ejercicio3;
+
+public interface IInstalacionDeportiva {
+	int getTipoDeInstalacion();
+}
+
