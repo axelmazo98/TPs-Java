8fe461bc8b73a96b8843d3d9ad9c2ca088b9ecc1
Joaquingigena <joagigena-11@hotmail.com>
2023-08-24 15:06:43 -0300

    Gets, sets y constructores de productoCongelado

--- src/Ejercicio2/productoCongelado.java
+++ src/Ejercicio2/productoCongelado.java
@@ -4,5 +4,28 @@ public class productoCongelado extends Producto{
 
 	private int tempCongelacionRecomendada;
 
+	//Constructores
+	public productoCongelado() {
+		super();
+		this.tempCongelacionRecomendada=0;
+	}
+	
+	public productoCongelado(String fechaCaducidad,int numLote, int tempCongelacionRecomendada) {
+		super(fechaCaducidad,numLote);
+		this.tempCongelacionRecomendada= tempCongelacionRecomendada;
+	}
+	
+	//Gets y Sets
+	public int getTempCongelacionRecomendada() {
+		return tempCongelacionRecomendada;
+	}
+
+	public void setTempCongelacionRecomendada(int tempCongelacionRecomendada) {
+		this.tempCongelacionRecomendada = tempCongelacionRecomendada;
+	}
+	
+	
+	
+
 }
 
