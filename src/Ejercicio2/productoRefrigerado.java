ad21ab1d26fb4e49738c9b6931888506249c3815
axel mazo <axel.98.mazo@gmail.com>
2023-08-24 14:44:07 -0300

    aporte 7y8 Ej2

--- src/Ejercicio2/productoRefrigerado.java
+++ src/Ejercicio2/productoRefrigerado.java
@@ -1,7 +1,29 @@
 package Ejercicio2;
 
-public class productoRefrigerado {
+public class productoRefrigerado extends Producto {
 
 	//código del organismo de supervisión alimentaria
 	private int codOrganismo;
+	
+	public productoRefrigerado() {
+		super();
+		codOrganismo = 0;
+	}
+	
+	public productoRefrigerado(String fechaCaducidad, int numeroLote, int codOrganismo) {
+        super(fechaCaducidad, numeroLote);
+        this.codOrganismo = codOrganismo;
+    }
+
+	public int getCodOrganismo() {
+		return codOrganismo;
+	}
+
+	public void setCodOrganismo(int codOrganismo) {
+		this.codOrganismo = codOrganismo;
+	}
+	
+	
 }
+
+
