5dbc244a7ae50b68452a39f0e14e91296a4f0c4f
axel mazo <axel.98.mazo@gmail.com>
2023-08-27 18:01:08 -0300

    Comienzo TP3

---
+++ TP3_Grupo_6/src/Ejercicio1/Persona.java
@@ -0,0 +1,28 @@
+package Ejercicio1;
+
+public class Persona {
+
+	private String Nombre;
+	private String Apellido;
+	private int DNI;
+	public String getNombre() {
+		return Nombre;
+	}
+	public void setNombre(String nombre) {
+		Nombre = nombre;
+	}
+	public String getApellido() {
+		return Apellido;
+	}
+	public void setApellido(String apellido) {
+		Apellido = apellido;
+	}
+	public int getDNI() {
+		return DNI;
+	}
+	public void setDNI(int dNI) {
+		DNI = dNI;
+	}
+	
+	
+}
