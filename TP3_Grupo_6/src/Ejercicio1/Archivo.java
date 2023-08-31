f5f4ecc1e9a8598eb9545eb27ff025944499612e
MNRocha <mnrrocha@hotmail.com>
2023-08-31 02:19:08 -0300

    TP3 - Puntos 6 y 7

--- TP3_Grupo_6/src/Ejercicio1/Archivo.java
+++ TP3_Grupo_6/src/Ejercicio1/Archivo.java
@@ -1,6 +1,9 @@
 package Ejercicio1;
 
+import java.io.BufferedWriter;
 import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
 
 public class Archivo {
 	private String ruta;
@@ -34,6 +37,35 @@ public class Archivo {
 	
 	public void setRuta(String ruta) {
 		this.ruta = ruta;
+	}
+	
+	/// MÉTODOS
+	
+	public boolean crearArchivo() {
+		FileWriter escritura;
+			try {
+				escritura = new FileWriter(ruta, true);
+				escritura.write("");
+				escritura.close();
+				return true;
+			} catch(IOException e) {
+				e.printStackTrace();
+				System.out.println("No se pudo crear el archivo");
+			}
+			return false;
+	}
+	
+	public void escribirLineas(String texto) {
+		try {
+			FileWriter entrada = new FileWriter(ruta, true);
+			BufferedWriter buffer = new BufferedWriter(entrada);
+			buffer.write(texto);
+			buffer.close();
+			entrada.close();
+		} catch (IOException e) {
+			e.printStackTrace();
+			System.out.println("No se pudo escribir la línea");
+		}
 	}
 
 }
