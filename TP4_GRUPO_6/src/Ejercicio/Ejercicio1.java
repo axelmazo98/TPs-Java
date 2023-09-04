97a2c07fa4ba523c770d77396d3371288c5d4dc8
Nahuelcapo2001 <nahuel.sandovalcapo@gmail.com>
2023-09-04 19:45:49 -0300

    aportes 4 y 5 de carlos

--- TP4_GRUPO_6/src/Ejercicio/Ejercicio1.java
+++ TP4_GRUPO_6/src/Ejercicio/Ejercicio1.java
@@ -6,22 +6,93 @@ import java.awt.EventQueue;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.border.EmptyBorder;
+import java.awt.Window.Type;
+import java.awt.Font;
+import javax.swing.JLabel;
+import javax.swing.JButton;
+import javax.swing.JTextField;
 
 public class Ejercicio1 extends JFrame {
 
-
+	/**
+	 * 
+	 */
 	private static final long serialVersionUID = 1L;
+
 	private JPanel contentPane;
+	private JTextField textField;
+	private JTextField textField_1;
+	private JTextField textField_2;
+	private JTextField textField_3;
 
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					Ejercicio1 frame = new Ejercicio1();
+					frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
 
+	/**
+	 * Create the frame.
+	 */
 	public Ejercicio1() {
+		setFont(new Font("Arial", Font.BOLD, 13));
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		setBounds(100, 100, 450, 300);
-		setTitle("Ejercicio 1");
+		setTitle("Contactos");
+		getContentPane().setLayout(null);
 		contentPane = new JPanel();
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
-		contentPane.setLayout(new BorderLayout(0, 0));
 		setContentPane(contentPane);
+		contentPane.setLayout(null);
+		
+		JLabel lblNewLabel = new JLabel("Nombre");
+		lblNewLabel.setBounds(68, 35, 83, 21);
+		contentPane.add(lblNewLabel);
+		
+		textField = new JTextField();
+		textField.setBounds(174, 35, 182, 20);
+		contentPane.add(textField);
+		textField.setColumns(10);
+		
+		JLabel lblNewLabel_1 = new JLabel("Apellido");
+		lblNewLabel_1.setBounds(68, 95, 46, 14);
+		contentPane.add(lblNewLabel_1);
+		
+		textField_1 = new JTextField();
+		textField_1.setBounds(174, 92, 182, 20);
+		contentPane.add(textField_1);
+		textField_1.setColumns(10);
+		
+		JLabel lblNewLabel_2 = new JLabel("Teléfono");
+		lblNewLabel_2.setBounds(68, 148, 69, 14);
+		contentPane.add(lblNewLabel_2);
+		
+		textField_2 = new JTextField();
+		textField_2.setBounds(174, 145, 182, 20);
+		contentPane.add(textField_2);
+		textField_2.setColumns(10);
+		
+		JLabel lblNewLabel_3 = new JLabel("Fec. Nacimiento");
+		lblNewLabel_3.setBounds(68, 202, 96, 14);
+		contentPane.add(lblNewLabel_3);
+		
+		textField_3 = new JTextField();
+		textField_3.setBounds(174, 199, 182, 20);
+		contentPane.add(textField_3);
+		textField_3.setColumns(10);
+		
+		
+		setVisible(true);
 	}
 
 }
--- TP4_GRUPO_6/src/Ejercicio/Main.java
+++ TP4_GRUPO_6/src/Ejercicio/Main.java
@@ -51,20 +51,33 @@ public class Main extends JFrame{
 		lblGrupo.setBounds(10, 0, 236, 60);
 		contentPane.add(lblGrupo);
 		
-		JButton button1 = new JButton();
-		button1.setText("Ejercicio 1");
-		button1.setBounds(150,70,120,30);
-		contentPane.add(button1);
+		JButton btnEjercicio1 = new JButton("Ejercicio 1");
+		btnEjercicio1.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent arg0) {
+				Ejercicio1 ej1 = new Ejercicio1();
+			}
+		});
+		btnEjercicio1.setBounds(157, 98, 116, 23);
+		contentPane.add(btnEjercicio1);
 		
-		JButton button2 = new JButton();
-		button2.setText("Ejercicio 2");
-		button2.setBounds(150,120,120,30);
-		contentPane.add(button2);
 		
-		JButton button3 = new JButton();
-		button3.setText("Ejercicio 3");
-		button3.setBounds(150,170,120,30);
-		contentPane.add(button3);
+		JButton btnEjercicio2 = new JButton("Ejercicio 2");
+		btnEjercicio2.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				///Ejercicio2 ej2 = new Ejercicio2();
+			}
+		});
+		btnEjercicio2.setBounds(157, 132, 116, 23);
+		contentPane.add(btnEjercicio2);
+		
+		JButton btnEjercicio3 = new JButton("Ejercicio 3");
+		btnEjercicio3.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				///Ejercicio3 ej3 = new Ejercicio3();
+			}
+		});
+		btnEjercicio3.setBounds(157, 166, 116, 23);
+		contentPane.add(btnEjercicio3);
 		
 
 	}
