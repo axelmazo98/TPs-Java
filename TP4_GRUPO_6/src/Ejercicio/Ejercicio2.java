77461a6789c4a8929c05509288546cdba5be4947
axel mazo <axel.98.mazo@gmail.com>
2023-09-06 15:56:39 -0300

    Ejercicio 2 de carlos

---
+++ TP4_GRUPO_6/src/Ejercicio/Ejercicio2.java
@@ -0,0 +1,78 @@
+package Ejercicio;
+
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.border.EmptyBorder;
+import javax.swing.JFormattedTextField;
+import javax.swing.JTextPane;
+import java.awt.Color;
+import javax.swing.JTabbedPane;
+import javax.swing.border.TitledBorder;
+import javax.swing.JLabel;
+import javax.swing.JTextField;
+import javax.swing.JEditorPane;
+import javax.swing.JToolBar;
+import javax.swing.border.EtchedBorder;
+
+public class Ejercicio2 extends JFrame {
+
+	private JPanel contentPane;
+	private JTextField textField;
+	private JTextField textField_1;
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					Ejercicio2 frame = new Ejercicio2();
+					frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the frame.
+	 */
+	public Ejercicio2() {
+		setTitle("Promedio");
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setBounds(100, 100, 450, 300);
+		contentPane = new JPanel();
+		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+
+		setContentPane(contentPane);
+		contentPane.setLayout(null);
+		
+		JPanel panel = new JPanel();
+		panel.setBorder(new TitledBorder(new EtchedBorder(EtchedBorder.LOWERED, new Color(255, 255, 255), new Color(160, 160, 160)), "Notas del estudiante", TitledBorder.LEADING, TitledBorder.TOP, null, new Color(0, 0, 0)));
+		panel.setBounds(44, 28, 247, 158);
+		contentPane.add(panel);
+		panel.setLayout(null);
+		
+		JLabel lblNewLabel = new JLabel("Nota 1: ");
+		lblNewLabel.setBounds(20, 40, 83, 14);
+		panel.add(lblNewLabel);
+		
+		textField = new JTextField();
+		textField.setBounds(100, 37, 86, 20);
+		panel.add(textField);
+		textField.setColumns(10);
+		
+		JLabel lblNewLabel_1 = new JLabel("Nota 2:");
+		lblNewLabel_1.setBounds(20, 68, 46, 14);
+		panel.add(lblNewLabel_1);
+		
+		textField_1 = new JTextField();
+		textField_1.setBounds(100, 65, 86, 20);
+		panel.add(textField_1);
+		textField_1.setColumns(10);
+	}
+}
