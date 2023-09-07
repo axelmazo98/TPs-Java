58b4b2031ae016680cae84a618231a793726a4cc
MNRocha <mnrrocha@hotmail.com>
2023-09-07 01:59:07 -0300

    Diseño y funcionalidad Boton Mostrar

--- TP4_GRUPO_6/src/Ejercicio/Ejercicio1.java
+++ TP4_GRUPO_6/src/Ejercicio/Ejercicio1.java
@@ -11,6 +11,8 @@ import java.awt.Font;
 import javax.swing.JLabel;
 import javax.swing.JButton;
 import javax.swing.JTextField;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
 
 public class Ejercicio1 extends JFrame {
 
@@ -20,10 +22,10 @@ public class Ejercicio1 extends JFrame {
 	private static final long serialVersionUID = 1L;
 
 	private JPanel contentPane;
-	private JTextField textField;
-	private JTextField textField_1;
-	private JTextField textField_2;
-	private JTextField textField_3;
+	private JTextField txtNombre;
+	private JTextField txtApellido;
+	private JTextField txtTelefono;
+	private JTextField txtFechaNac;
 
 	/**
 	 * Launch the application.
@@ -47,7 +49,7 @@ public class Ejercicio1 extends JFrame {
 	public Ejercicio1() {
 		setFont(new Font("Arial", Font.BOLD, 13));
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		setBounds(100, 100, 450, 300);
+		setBounds(100, 100, 450, 445);
 		setTitle("Contactos");
 		getContentPane().setLayout(null);
 		contentPane = new JPanel();
@@ -55,44 +57,56 @@ public class Ejercicio1 extends JFrame {
 		setContentPane(contentPane);
 		contentPane.setLayout(null);
 		
-		JLabel lblNewLabel = new JLabel("Nombre");
-		lblNewLabel.setBounds(68, 35, 83, 21);
-		contentPane.add(lblNewLabel);
+		JLabel lblNombre = new JLabel("Nombre");
+		lblNombre.setBounds(68, 35, 83, 21);
+		contentPane.add(lblNombre);
 		
-		textField = new JTextField();
-		textField.setBounds(174, 35, 182, 20);
-		contentPane.add(textField);
-		textField.setColumns(10);
+		txtNombre = new JTextField();
+		txtNombre.setBounds(174, 35, 182, 20);
+		contentPane.add(txtNombre);
+		txtNombre.setColumns(10);
 		
-		JLabel lblNewLabel_1 = new JLabel("Apellido");
-		lblNewLabel_1.setBounds(68, 95, 46, 14);
-		contentPane.add(lblNewLabel_1);
+		JLabel lblApellido = new JLabel("Apellido");
+		lblApellido.setBounds(68, 95, 46, 14);
+		contentPane.add(lblApellido);
 		
-		textField_1 = new JTextField();
-		textField_1.setBounds(174, 92, 182, 20);
-		contentPane.add(textField_1);
-		textField_1.setColumns(10);
+		txtApellido = new JTextField();
+		txtApellido.setBounds(174, 92, 182, 20);
+		contentPane.add(txtApellido);
+		txtApellido.setColumns(10);
 		
-		JLabel lblNewLabel_2 = new JLabel("Teléfono");
-		lblNewLabel_2.setBounds(68, 148, 69, 14);
-		contentPane.add(lblNewLabel_2);
+		JLabel lblTelefono = new JLabel("Teléfono");
+		lblTelefono.setBounds(68, 148, 69, 14);
+		contentPane.add(lblTelefono);
 		
-		textField_2 = new JTextField();
-		textField_2.setBounds(174, 145, 182, 20);
-		contentPane.add(textField_2);
-		textField_2.setColumns(10);
+		txtTelefono = new JTextField();
+		txtTelefono.setBounds(174, 145, 182, 20);
+		contentPane.add(txtTelefono);
+		txtTelefono.setColumns(10);
 		
-		JLabel lblNewLabel_3 = new JLabel("Fec. Nacimiento");
-		lblNewLabel_3.setBounds(68, 202, 96, 14);
-		contentPane.add(lblNewLabel_3);
+		JLabel lblFechaNac = new JLabel("Fec. Nacimiento");
+		lblFechaNac.setBounds(68, 202, 96, 14);
+		contentPane.add(lblFechaNac);
 		
-		textField_3 = new JTextField();
-		textField_3.setBounds(174, 199, 182, 20);
-		contentPane.add(textField_3);
-		textField_3.setColumns(10);
+		txtFechaNac = new JTextField();
+		txtFechaNac.setBounds(174, 199, 182, 20);
+		contentPane.add(txtFechaNac);
+		txtFechaNac.setColumns(10);
+		
+		JLabel lblMostrarDatos = new JLabel("Los datos ingresados fueron: ");
+		lblMostrarDatos.setBounds(68, 288, 318, 97);
+		contentPane.add(lblMostrarDatos);
+		
+		JButton btnMostrar = new JButton("Mostrar");
+		btnMostrar.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent arg0) {
+				lblMostrarDatos.setText(lblMostrarDatos.getText() + "\n"  +  txtNombre.getText() + " " + txtApellido.getText());
+			}
+		});
+		btnMostrar.setBounds(252, 239, 104, 33);
+		contentPane.add(btnMostrar);
 		
 		
 		setVisible(true);
 	}
-
 }
