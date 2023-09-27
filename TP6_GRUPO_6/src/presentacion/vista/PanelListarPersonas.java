a358488724d7bc1b2aed31f0063516b6c7251ccb
MNRocha <mnrrocha@hotmail.com>
2023-09-27 19:21:23 -0300

    TP6 - Punto 4

--- TP6_GRUPO_6/src/presentacion/vista/PanelListarPersonas.java
+++ TP6_GRUPO_6/src/presentacion/vista/PanelListarPersonas.java
@@ -9,6 +9,8 @@ import java.awt.GridBagLayout;
 import java.awt.GridBagConstraints;
 import javax.swing.JLabel;
 import javax.swing.SwingConstants;
+import javax.swing.table.DefaultTableModel;
+import javax.swing.table.JTableHeader;
 import javax.swing.BoxLayout;
 import com.jgoodies.forms.layout.FormLayout;
 import com.jgoodies.forms.layout.ColumnSpec;
@@ -16,8 +18,13 @@ import com.jgoodies.forms.layout.RowSpec;
 import com.jgoodies.forms.layout.FormSpecs;
 import net.miginfocom.swing.MigLayout;
 import javax.swing.SpringLayout;
+import javax.swing.JTable;
+import java.awt.event.ComponentAdapter;
+import java.awt.event.ComponentEvent;
+import javax.swing.ListSelectionModel;
 
 public class PanelListarPersonas extends JPanel {
+	private JTable jtListaPersonas;
 
 	/**
 	 * Create the panel.
@@ -29,8 +36,27 @@ public class PanelListarPersonas extends JPanel {
 		lblLista.setHorizontalAlignment(SwingConstants.CENTER);
 		add(lblLista, BorderLayout.NORTH);
 		
-		JList jlListarPersonas = new JList();
-		add(jlListarPersonas, BorderLayout.CENTER);
-
+		jtListaPersonas = new JTable();
+		jtListaPersonas.setSelectionMode(ListSelectionModel.MULTIPLE_INTERVAL_SELECTION);
+		jtListaPersonas.setModel(new DefaultTableModel(
+			new Object[][] {
+			},
+			new String[] {
+				"Nombre"
+			}
+		) {
+			Class[] columnTypes = new Class[] {
+				String.class
+			};
+			public Class getColumnClass(int columnIndex) {
+				return columnTypes[columnIndex];
+			}
+		});
+		
+		/*DefaultTableModel tablaDefecto = new DefaultTableModel();
+		String[] columnas = new String[] {"Nombre", "Apellido", "DNI"};
+		tablaDefecto.setColumnIdentifiers(columnas);
+		jtListaPersonas.setModel(tablaDefecto);*/
+		add(jtListaPersonas, BorderLayout.CENTER);
 	}
 }
