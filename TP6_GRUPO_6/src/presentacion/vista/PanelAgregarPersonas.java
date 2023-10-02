e08b60952bab1affc89a5db48760331281408eda
carlosjesus <cjesus@nativaconsulting.com>
2023-10-01 22:34:16 -0500

    validacion de input type string & int

--- TP6_GRUPO_6/src/presentacion/vista/PanelAgregarPersonas.java
+++ TP6_GRUPO_6/src/presentacion/vista/PanelAgregarPersonas.java
@@ -5,6 +5,8 @@ import javax.swing.JLabel;
 import javax.swing.JTextField;
 import javax.swing.JButton;
 import java.awt.event.ActionListener;
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
 import java.awt.event.ActionEvent;
 
 public class PanelAgregarPersonas extends JPanel {
@@ -29,16 +31,19 @@ public class PanelAgregarPersonas extends JPanel {
 		add(lblDni);
 		
 		txtNombre = new JTextField();
+		ValidarTipoDeDatoString(txtNombre);
 		txtNombre.setBounds(141, 24, 86, 20);
 		add(txtNombre);
 		txtNombre.setColumns(10);
 		
 		txtApellido = new JTextField();
+		ValidarTipoDeDatoString(txtApellido);
 		txtApellido.setBounds(141, 60, 86, 20);
 		add(txtApellido);
 		txtApellido.setColumns(10);
 		
 		txtDni = new JTextField();
+		ValidarTipoDatoInt(txtDni);
 		txtDni.setBounds(141, 100, 86, 20);
 		add(txtDni);
 		txtDni.setColumns(10);
@@ -62,5 +67,54 @@ public class PanelAgregarPersonas extends JPanel {
 
 	public JButton getBtnAceptar() {
 		return btnAceptar;
+	}
+	
+	public void ValidarTipoDeDatoString(JTextField txtField) {
+			
+			txtField.addKeyListener(new KeyListener() {
+	            @Override
+	            public void keyTyped(KeyEvent e) {
+	                char c = e.getKeyChar();
+	             // Verificar si el carácter es una letra o un espacio en blanco
+	                if (!(Character.isLetter(c) || c == KeyEvent.VK_SPACE ||
+	                        (c == KeyEvent.VK_BACK_SPACE) || (c == KeyEvent.VK_DELETE))) {
+	                    e.consume(); // Ignorar el evento si no es una letra o espacio
+	                }
+	            }
+	
+	            @Override
+	            public void keyPressed(KeyEvent e) {
+	                // No necesitas implementar este método, pero debe estar presente debido a la interfaz KeyListener
+	            }
+	
+	            @Override
+	            public void keyReleased(KeyEvent e) {
+	                // No necesitas implementar este método, pero debe estar presente debido a la interfaz KeyListener
+	            }
+	        });
+			
+	}
+	
+	public void ValidarTipoDatoInt(JTextField txtField) {
+		txtField.addKeyListener(new KeyListener() {
+            @Override
+            public void keyTyped(KeyEvent e) {
+                char c = e.getKeyChar();
+                if ((Character.isLetter(c) || (c == KeyEvent.VK_SPACE) || (c == KeyEvent.VK_DELETE))) {
+                    e.consume(); // Ignorar el evento si no es un numero
+                }
+            }
+
+            @Override
+            public void keyPressed(KeyEvent e) {
+                // No necesitas implementar este método, pero debe estar presente debido a la interfaz KeyListener
+            }
+
+            @Override
+            public void keyReleased(KeyEvent e) {
+                // No necesitas implementar este método, pero debe estar presente debido a la interfaz KeyListener
+            }
+        });
+		
 	}
 }
